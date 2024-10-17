23233088787d4878685189204eeabb06067f9362
robfs <robfoxsimms@gmail.com>
2024-10-18 00:00:23 +0100

    update git keymaps

--- init.lua
+++ init.lua
@@ -96,14 +96,13 @@ vim.g.have_nerd_font = true
 -- [[ Setting options ]]
 require 'options'
 
--- [[ Basic Keymaps ]]
-require 'keymaps'
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 require 'lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- [[ Basic Keymaps ]]
+require 'keymaps'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
