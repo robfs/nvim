7afb2c5944b4db5e470feb9dec1cd34b359204ff
robfs <robfoxsimms@gmail.com>
2024-10-19 17:42:16 +0100

    Add neoclip and ctrl-v keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -53,4 +53,6 @@ vim.keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { desc = '[G]it [p]ull' })
 vim.keymap.set('n', '<leader>gP', ':Neogit push<CR>', { desc = '[G]it [P]ush' })
 vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<CR>', { desc = '[G]it [b]ranches' })
 vim.keymap.set('n', '<leader>gB', neogit.open, { desc = '[G]it [B]lame' })
+-- Keymaps for neoclip
+vim.keymap.set({ 'n', 'v' }, '<C-v>', ':Telescope neogit<CR>')
 -- vim: ts=2 sts=2 sw=2 et
