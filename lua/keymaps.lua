637a5c264d08f02865fee5d238dd0ec31f7031b7
robfs <robfoxsimms@gmail.com>
2024-10-19 17:49:08 +0100

    Add spectre for assisted find and replace

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -53,6 +53,18 @@ vim.keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { desc = '[G]it [p]ull' })
 vim.keymap.set('n', '<leader>gP', ':Neogit push<CR>', { desc = '[G]it [P]ush' })
 vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<CR>', { desc = '[G]it [b]ranches' })
 vim.keymap.set('n', '<leader>gB', neogit.open, { desc = '[G]it [B]lame' })
+
+-- Keymaps for Spectre
+vim.keymap.set('n', '<leader>tf', '<cmd>lua require("spectre").toggle()<CR>', {
+  desc = 'Toggle Spectre ([F]ind)',
+})
+vim.keymap.set({ 'n', 'v' }, '<leader>fw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+  desc = '[F]ind current word',
+})
+vim.keymap.set('n', '<leader>fp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+  desc = '[F]ind in current file',
+})
+
 -- Keymaps for neoclip
 vim.keymap.set({ 'n', 'v' }, '<C-v>', ':Telescope neogit<CR>')
 -- vim: ts=2 sts=2 sw=2 et
