e869352056c7ac4e1d72f9a50b226cbca0a095df
robfs <robfoxsimms@gmail.com>
2025-08-06 18:00:06 +0100

    add clipboard sync to options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,11 @@
 -- vim.opt.shiftwidth = 4
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = 'unnamedplus'
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.o.scrolloff = 10
 
-vim.g.python3_host_prog = "~/nvimvenv/.venv/bin/python"
+vim.g.python3_host_prog = '~/nvimvenv/.venv/bin/python'
+
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
