06b7e463c0368a70d495ca9e7c55b609200da526
robfs <robfoxsimms@gmail.com>
2025-08-16 17:13:36 +0100

    update obsidian config to use VAULT variable for workspace config

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,27 +1,14 @@
 return {
   'obsidian-nvim/obsidian.nvim',
-  config = function()
-    local config = {
-      ui = { enable = false },
-      completion = { blink = true, nvim_cmp = false },
-      picker = { name = 'snacks.pick' },
-      legacy_commands = false,
-    }
-
-    local workspace_config = {}
-    local work_path = vim.fn.expand '~/OneDrive - USS/USS/vault'
-    local has_work_vault = vim.fn.isdirectory(work_path)
-
-    if has_work_vault == 1 then
-      workspace_config = {
-        workspaces = { { name = 'ussim', path = '~/OneDrive - USS/USS/vault' } },
-      }
-    else
-      workspace_config = {
-        workspaces = { { name = 'personal', path = '~/vaults/personal' } },
-      }
-    end
-
-    require('obsidian').setup(vim.tbl_extend('force', config, workspace_config))
-  end,
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    workspaces = {
+      { name = 'personal', path = os.getenv 'VAULT' or vim.fn.expand '~/vault' },
+    },
+    ui = { enable = false },
+    completion = { blink = true, nvim_cmp = false },
+    picker = { name = 'snacks.pick' },
+    legacy_commands = false,
+  },
 }
