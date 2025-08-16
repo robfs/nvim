16fdcda58274d2fbdfddf6f1ebedf627fc32f081
Rob Fox <rsimms@uss.co.uk>
2025-08-16 20:53:01 +0100

    disable luarocks in lazy config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,6 +28,7 @@ require('lazy').setup {
     -- import your plugins
     { import = 'custom.plugins' },
   },
+  rocks = { enabled = false },
   -- -- Configure any other settings here. See the documentation for more details.
   -- -- colorscheme that will be used when installing plugins.
   -- install = { colorscheme = { 'catppuccin' } },
