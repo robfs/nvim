e48839ae7648af61e2b7c6651ffba92c902585bc
Rob Fox <rsimms@uss.co.uk>
2025-08-16 20:52:50 +0100

    add yaml to treesitter parsers

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
     config = function()
       require('nvim-treesitter.configs').setup {
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'python', 'regex', 'bash', 'html' },
+        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'python', 'regex', 'bash', 'html', 'yaml' },
         auto_install = false,
         highlight = {
           enable = true,
