6c4ed9eaa65b69d130f007da2b9c5c4d18e9bd54
Rob Fox <rsimms@uss.co.uk>
2025-08-16 22:17:29 +0100

    move pairs to blink instead of autopairs

--- lua/custom/plugins/pairs.lua
+++ lua/custom/plugins/pairs.lua
@@ -1,9 +1,36 @@
 return {
   {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = true,
-    opts = {}, -- for passing setup options
-    -- this is equivalent to setup({}) function
+    'saghen/blink.pairs',
+    version = '*', -- (recommended) only required with prebuilt binaries
+    dependencies = 'saghen/blink.download',
+    --- @module 'blink.pairs'
+    --- @type blink.pairs.Config
+    opts = {
+      mappings = {
+        enabled = true,
+        disabled_filetypes = {},
+        pairs = {},
+      },
+      highlights = {
+        enabled = true,
+        -- requires require('vim._extui').enable({}), otherwise has no effect
+        -- cmdline = true,
+        groups = {
+          'BlinkPairsOrange',
+          'BlinkPairsPurple',
+          'BlinkPairsBlue',
+        },
+        -- unmatched_group = 'BlinkPairsUnmatched',
+
+        -- highlights matching pairs under the cursor
+        matchparen = {
+          enabled = true,
+          -- known issue where typing won't update matchparen highlight, disabled by default
+          -- cmdline = false,
+          group = 'BlinkPairsMatchParen',
+        },
+      },
+      debug = false,
+    },
   },
 }
